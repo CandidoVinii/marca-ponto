4c1cf1b1f8d8ac600f8eca13ba39789904f405e7
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-01-28 14:28:40 -0300

    feat: adicionado metodo get com o endpoint /get-points para efetuar o dowload do pdf

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\PontoController;
+use App\Http\Controllers\PontoPdfController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/registry-point', [PontoController::class, 'registryPoint']);
+
+Route::get('/get-points', [PontoPdfController::class, 'generatePDF']);
