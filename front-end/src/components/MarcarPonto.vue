c698f1561b68baccc8e6537f15cb710094728d69
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-01-29 11:45:46 -0300

    feat: criado componente MarcarPonto com o esqueleto do view

---
+++ front-end/src/components/MarcarPonto.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="input-group">
+        <label for="name">Nome:</label>
+        <input type="text" id="name" />
+        <label for="chegada">Chegada:</label>
+        <input type="datetime-local" id="chegada" />
+        <label for="saida">Saída:</label>
+        <input type="datetime-local" id="saida" />
+      </div>
+    </form>
+    <div class="button-group">
+      <button @click="">Enviar</button>
+      <button @click="">Exportar PDF</button>
+    </div>
+  </div>
+</template>
