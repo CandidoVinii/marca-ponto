1cc41f70e6fd2aae77d44976e58fff94382336e9
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-01-29 11:55:52 -0300

    Feat: adicionado lógica para capturar o valor e fazer a requisição, criado lógica para desabilitar o botao caso tenha algum valor vazio

--- front-end/src/components/MarcarPonto.vue
+++ front-end/src/components/MarcarPonto.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="container">
-    <form>
+    <form @submit.prevent="submitForm">
       <div class="input-group">
         <label for="name">Nome:</label>
-        <input type="text" id="name" />
+        <input type="text" v-model="form.name" id="name" />
         <label for="chegada">Chegada:</label>
-        <input type="datetime-local" id="chegada" />
+        <input type="datetime-local" v-model="form.chegada" id="chegada" />
         <label for="saida">Saída:</label>
-        <input type="datetime-local" id="saida" />
+        <input type="datetime-local" v-model="form.saida" id="saida" />
       </div>
     </form>
     <div class="button-group">
-      <button @click="">Enviar</button>
+      <button :disabled="buttonDisabled" @click="submitForm">Enviar</button>
       <button @click="">Exportar PDF</button>
     </div>
   </div>
 </template>
+
+<script>
+import Ponto from '@/services/ponto.js';
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        chegada: '',
+        saida: '',
+      },
+    };
+  },
+  computed: {
+    buttonDisabled() {
+      const data = this.form;
+      return !data.name || !data.chegada || !data.saida;
+    },
+  },
+  methods: {
+    submitForm() {
+      Ponto.marcar(this.form);
+      this.form = {};
+    },
+  },
+};
+</script>
